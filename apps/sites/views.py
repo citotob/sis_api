661a05267170231a82c66df061cd34d43921ca3b
christopher <erdell_tog@yahoo.com>
2020-10-24 15:22:21 +0700

    Update views.py

--- apps/sites/views.py
+++ apps/sites/views.py
@@ -894,11 +894,17 @@ def getDashboard(request):
 
         aiCount = Odp.objects.all().count()
         aiTech = Odp.objects.only('teknologi').distinct('teknologi')
+        #aiOperational = {
+        #    "count": aiCount,
+        #    "FO": 0,
+        #    "VSAT": 0,
+        #    "RL": 0
+        #}
         aiOperational = {
             "count": aiCount,
-            "FO": 0,
+            "FIBER OPTIK": 0,
             "VSAT": 0,
-            "RL": 0
+            "RADIO LINK": 0
         }
         for x in list(aiTech):
             aiOperational.update({
