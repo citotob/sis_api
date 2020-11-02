29dd8907e8c46a4f13310296e5e35a1a2b011f73
christopher <erdell_tog@yahoo.com>
2020-11-02 14:39:30 +0700

    1

---
+++ apps/sites/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+#from rest_framework.authtoken import views as tokenview
+
+
+urlpatterns = [
+    # path('login/', tokenview.obtain_auth_token),
+    path('addbatch/', views.addbatch),
+    path('addsite/', views.addsite),
+    path('editbatch/', views.editbatch),
+    path('uploadsite/', views.uploadsite),
+    path('getbatch/', views.getbatch),
+    path('getallbatch/', views.getallbatch),
+    
+    path('dashboard/', views.getDashboard),
+
+    path('uploadsiteoffair/', views.uploadsiteoffair),
+    path('getoffair/', views.getsiteoffair),
+
+    path('calculatevendorscore/', views.calculatevendorscore),
+]
