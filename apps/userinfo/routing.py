468a02a8c7a2bf9d20b77634f33211206804f5bd
christopher <erdell_tog@yahoo.com>
2020-12-22 18:11:28 +0700

    Update routing.py

--- apps/userinfo/routing.py
+++ apps/userinfo/routing.py
@@ -5,13 +5,13 @@
 from django.urls import path, re_path
 from . import consumers
 
-websocket_urlpatterns = [
-
-    re_path(r'ws/$', consumers.ChatConsumer.as_asgi()),
-    #re_path(r'ws/$', consumers.ChatConsumer),
-]
-
 #websocket_urlpatterns = [
-#    path('ws/', consumers.ChatConsumer),
-#    #url(r'^ws/', consumers.ChatConsumer),
+
+#    re_path(r'ws/$', consumers.ChatConsumer.as_asgi()),
+#    #re_path(r'ws/$', consumers.ChatConsumer),
 #]
+
+websocket_urlpatterns = [
+    path('ws/', consumers.ChatConsumer),
+#    #url(r'^ws/', consumers.ChatConsumer),
+]
