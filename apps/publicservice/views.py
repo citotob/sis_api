bcba56cc3bb2c3b82376692fdaab1081a7a19da2
christopher <erdell_tog@yahoo.com>
2020-12-30 15:09:44 +0700

    Update views.py
    email executive

--- apps/publicservice/views.py
+++ apps/publicservice/views.py
@@ -179,65 +179,85 @@ class publicServiceAPI(ModelViewSet):
         except Exception as e:
             return CustomResponse().base(success=False, message=str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def getDashboard(request):
+    def getLaporan(self, request, format=None):
         try:
+            #bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni",
+            #        "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+            
+            #req = request.body.decode("utf-8")
+            #data = json.loads(req)
+            #month = data['month']
+            #year = data['year']
+
+            #pastMonth = data['month'] - 1
+            #pastYear = year
+
+            #if pastMonth == 0:
+            #    pastMonth = 12
+            #    pastYear = year-1
+
+            #if month is None or year is None:
+            #    return CustomResponse.ok(message='Need Json Body "month" & "year"')
+
             vendorCount = vendor.objects.all().count()
             activeUserCount = UserInfo.objects(status='Aktif').count()
             requestedUserCount = UserInfo.objects(status='Belum Terverifikasi').count()
             batchCount = batch.objects.all().count()
             siteCount = site_matchmaking.objects(batchid__exists=True).count()
             rfiCount = vendor_application.objects.all().count()
-            #siteNonBatchCount = 0
-            #vendorListQuery = vendor.objects.all()
-            #vendorList = VendorScoreSerializer(vendorListQuery, many=True)
+            """
+            siteNonBatchCount = 0
+            vendorListQuery = vendor.objects.all()
+            vendorList = VendorScoreSerializer(vendorListQuery, many=True)
 
             aiCount = Odp.objects.filter(teknologi__in=['VSAT','FO','RL']).count()
-            #aiTech = Odp.objects.only('teknologi').distinct('teknologi')
-            #aiOperational = {
-            #    "count": aiCount,
-            #    "FO": 0,
-            #    "VSAT": 0,
-            #    "RL": 0
-            #}
-            #for x in list(aiTech):
-            #    xx=x
-            #    aiOperational.update({
-            #        xx: Odp.objects(teknologi=x).count()
-            #    })
+            aiTech = Odp.objects.only('teknologi').distinct('teknologi')
+            aiOperational = {
+                "count": aiCount,
+                "FO": 0,
+                "VSAT": 0,
+                "RL": 0
+            }
+            
+            for x in list(aiTech):
+                xx=x
+                aiOperational.update({
+                    xx: Odp.objects(teknologi=x).count()
+                })
 
-            #recommendTech = rekomendasi_teknologi.objects.only(
-            #    'teknologi').distinct('teknologi')
-            #siteAICount = site.objects.all().count()
-            #aiNew = {
-            #    "count": siteAICount,
-            #    "FO": 0,
-            #    "VSAT": 0,
-            #    "RL": 0
-            #}
-            #for x in list(recommendTech):
-            #    query = rekomendasi_teknologi.objects(teknologi=x).scalar('id')
-            #    aiNew.update({
-            #        x: site.objects(rekomendasi_teknologi__in=query).count()
-            #    })
+            recommendTech = rekomendasi_teknologi.objects.only(
+                'teknologi').distinct('teknologi')
+            siteAICount = site.objects.all().count()
+            aiNew = {
+                "count": siteAICount,
+                "FO": 0,
+                "VSAT": 0,
+                "RL": 0
+            }
+            for x in list(recommendTech):
+                query = rekomendasi_teknologi.objects(teknologi=x).scalar('id')
+                aiNew.update({
+                    x: site.objects(rekomendasi_teknologi__in=query).count()
+                })
 
             date = datetime.now()
             listMonth = calendar.month_abbr[1:13]
-            #reportSite = {}
-            #reportRFi = {}
-            #for x in range(11, -1, -1):
-            #    dateReport = date - relativedelta(months=x)
-            #    year = dateReport.year
-            #    month = dateReport.month
-            #    lastDate = calendar.monthrange(year=year, month=month)[1]
-            #    gte = datetime(year, month, 1, 00, 00, 00)
-            #    lte = datetime(year, month, lastDate, 23, 59, 59)
-            #    reportSite.update({
-            #        f'{listMonth[month-1]} {year}': batch.objects(created_at__gte=gte, created_at__lte=lte).count()
-            #    })
-            #    reportRFi.update({
-            #        f'{listMonth[month-1]} {year}': vendor_application.objects(created_at__gte=gte, created_at__lte=lte).count()
-            #    })
-
+            reportSite = {}
+            reportRFi = {}
+            for x in range(11, -1, -1):
+                dateReport = date - relativedelta(months=x)
+                year = dateReport.year
+                month = dateReport.month
+                lastDate = calendar.monthrange(year=year, month=month)[1]
+                gte = datetime(year, month, 1, 00, 00, 00)
+                lte = datetime(year, month, lastDate, 23, 59, 59)
+                reportSite.update({
+                    f'{listMonth[month-1]} {year}': batch.objects(created_at__gte=gte, created_at__lte=lte).count()
+                })
+                reportRFi.update({
+                    f'{listMonth[month-1]} {year}': vendor_application.objects(created_at__gte=gte, created_at__lte=lte).count()
+                })
+            
             result = {
                 "vendor": vendorCount,
                 "active_user": activeUserCount,
@@ -245,146 +265,40 @@ class publicServiceAPI(ModelViewSet):
                 "batch": batchCount,
                 "site": siteCount,
                 "rfi": rfiCount,
-                #"site_not_batch": siteNonBatchCount,
-                #"vendor_list": json.loads(json.dumps(vendorList.data, default=str)),
-                #"running_ai": aiOperational,
-                #"new_ai": aiNew,
-                #"report": {
-                #    "site": reportSite,
-                #    "rfi": reportRFi
-                #}
+                "site_not_batch": siteNonBatchCount,
+                "vendor_list": json.loads(json.dumps(vendorList.data, default=str)),
+                "running_ai": aiOperational,
+                "new_ai": aiNew,
+                "report": {
+                    "site": reportSite,
+                    "rfi": reportRFi
+                }
             }
+            """
+            executive = [x.email for x in UserInfo.objects.filter(
+                role=ObjectId('5f73fe3428751d590d835267'))]
 
-            return Response.ok(
-                values=result
-            )
+            subject = 'Laporan'
+            text_content = ''
+            template = 'email/executive/EmailLaporan.html'
+            d = {#'username': user.username, 
+                    'media_url': settings.URL_MEDIA,
+                    'url_login': settings.URL_LOGIN,
+                    'vendorCount': vendorCount,
+                    'activeUserCount': activeUserCount,
+                    'requestedUserCount': requestedUserCount,
+                    'batchCount': batchCount,
+                    'siteCount': siteCount,
+                    'rfiCount': rfiCount
+                }
+            email_sender = settings.EMAIL_ADMIN
+            email_receipient = executive
+            send_mail(subject,text_content,template,d,email_sender,email_receipient)
 
+            return CustomResponse.ok(message='Berhasil')
+        except TypeError as e:
+            return CustomResponse().base(success=False, message=str(e), status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response.badRequest(
-                values=[],
-                message=str(e)
-            )
-
-    def getLaporan(self, request, format=None):
-        #try:
-        #bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni",
-        #        "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-        
-        #req = request.body.decode("utf-8")
-        #data = json.loads(req)
-        #month = data['month']
-        #year = data['year']
-
-        #pastMonth = data['month'] - 1
-        #pastYear = year
-
-        #if pastMonth == 0:
-        #    pastMonth = 12
-        #    pastYear = year-1
-
-        #if month is None or year is None:
-        #    return CustomResponse.ok(message='Need Json Body "month" & "year"')
-
-        vendorCount = vendor.objects.all().count()
-        activeUserCount = UserInfo.objects(status='Aktif').count()
-        requestedUserCount = UserInfo.objects(status='Belum Terverifikasi').count()
-        batchCount = batch.objects.all().count()
-        siteCount = site_matchmaking.objects(batchid__exists=True).count()
-        rfiCount = vendor_application.objects.all().count()
-        """
-        siteNonBatchCount = 0
-        vendorListQuery = vendor.objects.all()
-        vendorList = VendorScoreSerializer(vendorListQuery, many=True)
-
-        aiCount = Odp.objects.filter(teknologi__in=['VSAT','FO','RL']).count()
-        aiTech = Odp.objects.only('teknologi').distinct('teknologi')
-        aiOperational = {
-            "count": aiCount,
-            "FO": 0,
-            "VSAT": 0,
-            "RL": 0
-        }
-        
-        for x in list(aiTech):
-            xx=x
-            aiOperational.update({
-                xx: Odp.objects(teknologi=x).count()
-            })
-
-        recommendTech = rekomendasi_teknologi.objects.only(
-            'teknologi').distinct('teknologi')
-        siteAICount = site.objects.all().count()
-        aiNew = {
-            "count": siteAICount,
-            "FO": 0,
-            "VSAT": 0,
-            "RL": 0
-        }
-        for x in list(recommendTech):
-            query = rekomendasi_teknologi.objects(teknologi=x).scalar('id')
-            aiNew.update({
-                x: site.objects(rekomendasi_teknologi__in=query).count()
-            })
-
-        date = datetime.now()
-        listMonth = calendar.month_abbr[1:13]
-        reportSite = {}
-        reportRFi = {}
-        for x in range(11, -1, -1):
-            dateReport = date - relativedelta(months=x)
-            year = dateReport.year
-            month = dateReport.month
-            lastDate = calendar.monthrange(year=year, month=month)[1]
-            gte = datetime(year, month, 1, 00, 00, 00)
-            lte = datetime(year, month, lastDate, 23, 59, 59)
-            reportSite.update({
-                f'{listMonth[month-1]} {year}': batch.objects(created_at__gte=gte, created_at__lte=lte).count()
-            })
-            reportRFi.update({
-                f'{listMonth[month-1]} {year}': vendor_application.objects(created_at__gte=gte, created_at__lte=lte).count()
-            })
-        
-        result = {
-            "vendor": vendorCount,
-            "active_user": activeUserCount,
-            "requested_user": requestedUserCount,
-            "batch": batchCount,
-            "site": siteCount,
-            "rfi": rfiCount,
-            "site_not_batch": siteNonBatchCount,
-            "vendor_list": json.loads(json.dumps(vendorList.data, default=str)),
-            "running_ai": aiOperational,
-            "new_ai": aiNew,
-            "report": {
-                "site": reportSite,
-                "rfi": reportRFi
-            }
-        }
-        """
-        executive = [x.email for x in UserInfo.objects.filter(
-            role=ObjectId('5f73fe3428751d590d835267'))]
-
-        subject = 'Laporan'
-        text_content = ''
-        template = 'email/executive/EmailLaporan.html'
-        d = {#'username': user.username, 
-                'media_url': settings.URL_MEDIA,
-                'url_login': settings.URL_LOGIN,
-                'vendorCount': vendorCount,
-                'activeUserCount': activeUserCount,
-                'requestedUserCount': requestedUserCount,
-                'batchCount': batchCount,
-                'siteCount': siteCount,
-                'rfiCount': rfiCount
-            }
-        email_sender = settings.EMAIL_ADMIN
-        email_receipient = executive
-        send_mail(subject,text_content,template,d,email_sender,email_receipient)
-
-        return CustomResponse.ok(message='Berhasil')
-        #except TypeError as e:
-        #    return CustomResponse().base(success=False, message=str(e), status=status.HTTP_404_NOT_FOUND)
-        #except Exception as e:
-        #    return CustomResponse().base(success=False, message=str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return CustomResponse().base(success=False, message=str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     
