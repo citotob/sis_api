b4bdba91951b011732e09f631536dc7dee5568a1
christopher <erdell_tog@yahoo.com>
2021-01-08 17:54:32 +0700

    Update routing.py

--- apps/notification/routing.py
+++ apps/notification/routing.py
@@ -1,10 +1,10 @@
 from django.urls import re_path, path
 from .consumer import NotifConsumer
 
-websocket_urlpatterns = [
-    re_path("ws/(?P<id>\w+)", NotifConsumer.as_asgi()),
-]
+#websocket_urlpatterns = [
+#    re_path("ws/(?P<id>\w+)", NotifConsumer.as_asgi()),
+#]
 
-# websocket_urlpatterns = [
-#     path('ws/', NotifConsumer),
-# ]
+websocket_urlpatterns = [
+     path('ws/(?P<id>\w+)', NotifConsumer),
+]
