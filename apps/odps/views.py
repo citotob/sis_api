b3bd75404451a2a35d1870cdfe2427adf2a0b887
christopher <erdell_tog@yahoo.com>
2021-02-22 14:23:40 +0700

    Update views.py

--- apps/odps/views.py
+++ apps/odps/views.py
@@ -556,7 +556,7 @@ def getRecommendTech(request):
                 success=False,
                 values=[],
                 message='Data tidak ada',
-                status=404
+                status=204
             )
 
     except Exception as e:
